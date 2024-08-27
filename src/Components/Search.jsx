1fd8716aefe1c07abb5fa90ee7d838ddfa06d67a
igurdeep13 <g.s.basantpuria@gmail.com>
2024-08-27 13:13:32 -0400

    add new changes to api

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,20 +1,16 @@
 import { useEffect, useState } from "react";
 
-
-
-const API_KEY = "1a9cd7d86fdc4a72b0d138a72082281b"
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 
 export default function Search() {
-  // Capturing the State Controlled Variable
+  // Capturing the State Controlled Variable.
   const [query, setQuery] = useState("pizza");
 
-  // it takes callback function and dependency list
+  // It takes callback function and dependency list.
   useEffect(() => {
     async function fetchFood() {
-      const res = await fetch(
-        `${URL}?query=${query}&apiKey=${API_KEY}`
-      );
+      const apiKey = import.meta.env.VITE_SPOONACULAR_API_KEY;
+      const res = await fetch(`${URL}?query=${query}&apiKey=${apiKey}`);
       const data = await res.json();
       console.log(data);
     }
