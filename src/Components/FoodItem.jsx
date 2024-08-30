1105ce5885fbfd12a5d0f35c0a25f7961ea83248
igurdeep13 <g.s.basantpuria@gmail.com>
2024-08-30 01:21:30 -0400

    add code

--- src/Components/FoodItem.jsx
+++ src/Components/FoodItem.jsx
@@ -1,7 +1,15 @@
-export default function FoodItem( {food}){
-    return <div>
-        <img src={food.image}/>
-        <h1>{food.title}</h1>
+import styles from "./foodItem.module.css";
+
+export default function FoodItem({ food }) {
+  return (
+    <div className={styles.itemContainer}>
+      <img className={styles.itemImg} src={food.image} />
+      <div className={styles.itemContentContainer}>
+        <p className={styles.itemContent}> {food.title}</p>
+      </div>
+      <div className={styles.itemButtonContainer}>
         <button>View Recipe</button>
+      </div>
     </div>
+  );
 }
